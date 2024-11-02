3c56f24b91d35fab80f22998bbf099ef43bb44dd
Mrec2 <marioteleco@outlook.com>
2024-11-02 23:29:03 +0100

    Backend Project created for Spin2Win. GitIgnore added.

---
+++ src/main/java/org/juanmario/Spin2WinBackendApplication.java
@@ -0,0 +1,13 @@
+package org.juanmario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spin2WinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spin2WinBackendApplication.class, args);
+	}
+
+}
